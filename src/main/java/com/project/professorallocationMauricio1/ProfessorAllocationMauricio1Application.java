6dfd444e191ea5ac8ce12104f896f5a111840e88
Mauricio Santos <mauriciogen3@hotmail.com>
2021-06-15 19:41:48 -0300

    ADICIONANDO CODIGO DE ALOCACAO DE PROFESSOR.

---
+++ src/main/java/com/project/professorallocationMauricio1/ProfessorAllocationMauricio1Application.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationMauricio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationMauricio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationMauricio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professorallocationMauricio1/ProfessorAllocationMauricio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationMauricio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationMauricio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
